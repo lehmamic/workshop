83f20ac7e6295b743c42bcb4ae592feb172a9e89
Michael Lehmann <michael.lehmann1@swisscom.com>
2020-08-26 14:15:24 +0200

    pod templates

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,9 +1,24 @@
-node {
-    stage("build") {
-        echo 'dotnet build'
-    }
+podTemplate(label: "dotnet-31",
+            cloud: "openshift",
+            inheritFrom: "maven",
+            containers: [
+                containerTemplate(name: "jnlp",
+                                image: "registry.redhat.io/dotnet/dotnet-31-jenkins-agent-rhel7:latest",
+                                ttyEnabled: true,
+                                envVars: [
+                                    envVar(key: "CONTAINER_HEAP_PERCENT", value: "0.25")
+                                ])
+            ],
+            volumes: [
+            ]) {
+    // node("dotnet-31") {
+    node() {
+        stage("build") {
+            echo 'dotnet build'
+        }
 
-    stage("test") {
-        echo 'dotnet test'
+        stage("test") {
+            echo 'dotnet test'
+        }
     }
 }
